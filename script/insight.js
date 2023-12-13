3a6c05d30ce67f53ab51d3b0e4a741e3901d722a
Jinseong Kwak <narlyeop@sk.com>
2023-12-13 21:23:18 +0900

    ADD : Basic Chart Drawing Added

--- script/insight.js
+++ script/insight.js
@@ -18,3 +18,63 @@ function getCookie(name) {
     }
     return null;
 }
+
+/* chart drawing */
+let previousChart = null;
+
+function drawChart(chartType, chartData, chartOptions, chartId) {
+    const ctx = document.getElementById(chartId).getContext('2d');
+
+    // Destroy existing chart instance if it exists
+    if (previousChart) {
+        previousChart.destroy();
+    }
+
+    // Create a new chart instance
+    previousChart = new Chart(ctx, {
+        type: chartType,
+        data: chartData,
+        options: chartOptions
+    });
+}
+
+function drawDiceHistogram(userId = '') {
+    const url = `http://127.0.0.1:8000/dice_histogram${userId ? '?user_id=' + userId : ''}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const labels = Object.keys(data);
+            const values = Object.values(data);
+            const chartData = {
+                labels: labels,
+                datasets: [{
+                    label: 'Number of Rolls',
+                    data: values,
+                    backgroundColor: 'rgba(54, 162, 235, 0.9)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                }]
+            };
+            const chartOptions = {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            };
+
+            drawChart('bar', chartData, chartOptions, 'diceHistogram');
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+
+document.getElementById('drawChartButton').addEventListener('click', function() {
+    const selectedGame = document.getElementById('gameSelection').value;
+    const userId = document.getElementById('userIdInput').value;
+
+    if (selectedGame === 'game1') {
+        drawDiceHistogram(userId);
+    }
+    // Add conditions for other games if needed
+});
